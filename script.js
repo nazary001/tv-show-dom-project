0a7b7b3463058e1fa34a6308fe42105b6d7a0613
nazary001 <nazary9911@gmail.com>
2022-08-02 17:27:29 +0300

    level 100

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
+let root = document.querySelector('#root');
+
 //You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+function formatSeazonAndNumber(number ){
+  return number >= 10 ? number : `0${number}`;
 }
+
+function makePageForEpisodes(episodeList) {
+  const root = document.getElementById("root");
+  episodeList.forEach(epis => {
+    let episode = document.createElement('div');
+    episode.className = 'episodes';
+    let episodeName = document.createElement('h1');
+    episodeName.className = 'episode_names';
+    let episodeImg = document.createElement('img');
+    episodeImg.className = 'episode_imgs';
+    let episodeSummary = document.createElement('p');
+    episodeSummary.className = 'episode_summarys';
+    
+    episodeName.innerText = `${epis.name} S${formatSeazonAndNumber(epis.season)}E${formatSeazonAndNumber(epis.number)}`;
+    episodeImg.src = epis.image.medium;
+    episodeSummary.innerHTML = epis.summary;
+
+    episode.appendChild(episodeName);
+    episode.appendChild(episodeImg);
+    episode.appendChild(episodeSummary);
+    root.appendChild(episode);
+    
+    console.log(episodeName.innerText);
+  })
+}
+
+
 
 window.onload = setup;
