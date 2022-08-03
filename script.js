9b4ba8dd9ef6d6718defe9f7ad682487b0b5466d
nazary001 <nazary9911@gmail.com>
2022-08-03 21:10:38 +0300

    level300

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const root = document.querySelector('#root');
 const searchInput = document.querySelector('#search_input');
 const numberOfEpisodes = document.querySelector('#number_of_episodes');
+const selectEpisode = document.querySelector('#episode_select');
 
 searchInput.addEventListener('keyup', startSearch);
+
 
 //You can edit ALL of the code here
 function setup() {
@@ -25,15 +27,19 @@ function makePageForEpisodes(episodeList) {
     episodeImg.className = 'episode_imgs';
     let episodeSummary = document.createElement('p');
     episodeSummary.className = 'episode_summarys';
+    let episodeOption = document.createElement('option');
+    episodeOption.value = epis.name;
     
     episodeName.innerText = `${epis.name} S${formatSeazonAndNumber(epis.season)}E${formatSeazonAndNumber(epis.number)}`;
     episodeImg.src = epis.image.medium;
     episodeSummary.innerHTML = epis.summary;
+    episodeOption.textContent = `S${formatSeazonAndNumber(epis.season)}E${formatSeazonAndNumber(epis.number)} - ${epis.name}`;
 
     episode.appendChild(episodeName);
     episode.appendChild(episodeImg);
     episode.appendChild(episodeSummary);
     root.appendChild(episode);
+    selectEpisode.appendChild(episodeOption);
   })
 }
 
@@ -46,6 +52,21 @@ function startSearch(){
   makePageForEpisodes(foundEpisodes);
   numberOfEpisodes.innerText = `Displaying ${foundEpisodes.length}/${allEpisodes.length} episodes`;
 }
+
+selectEpisode.addEventListener('change', (evnt) => {
+  if(evnt.target.value === 'all_episodes'){
+    const allEpisodes = getAllEpisodes();
+    makePageForEpisodes(allEpisodes);
+  }
+  else{
+    const allEpisodes = getAllEpisodes();
+    let foundEpisode = allEpisodes.filter((episode) => {
+    return evnt.target.value === episode.name ? true : false;
+    })
+  makePageForEpisodes(foundEpisode);
+  }
+  
+})
 
 
 window.onload = setup;
